9e030529261c20b62a6c63a7456561842ce1a087
Henrique Leme <henriqueleme.brito@outlook.com>
2024-02-14 22:56:32 -0300

    feat: header

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -2,9 +2,11 @@ import styles from "./style.module.css";
 
 export const Header = () => {
   return (
-    <div>
-      <div className={styles.links}>
+    <div className={styles.header}>
+      <div >
         <h1> Portfolio </h1>
+      </div>
+      <div >
         <h2>Home</h2>
         <h2>About me</h2>
         <h2>Projects</h2>
