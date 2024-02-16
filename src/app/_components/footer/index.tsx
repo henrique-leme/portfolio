06c81fbcc9f4c3916f05a1cd273be4c2fc416cff
Henrique Leme <henriqueleme.brito@outlook.com>
2024-02-15 22:57:55 -0300

    feat: footer

---
+++ src/app/_components/footer/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./style.module.css";
+
+export const Footer = () => {
+  return (
+    <div className={styles.footer}>
+        <div>
+            <div>henrique-leme</div>
+            <div>henrique-leme</div>
+            <div>henrique.lb@outlook.com</div>
+        </div>
+        <div>
+            Available for freelance oportunities
+        </div>
+      <div >
+        <div>Name</div>
+        <div>Email</div>
+        <div>Message</div>
+      </div>
+    </div>
+  );
+};
