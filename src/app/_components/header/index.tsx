1a22bd1cf2510fa792d5ee24c6f2529823a8c1df
Henrique Leme <henriqueleme.brito@outlook.com>
2024-02-27 19:42:04 -0300

    feat: mobile 1 header

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -1,21 +1,27 @@
-import styles from "./style.module.css";
+import styles from "./style.module.css"
 
 export const Header = () => {
   return (
     <nav className={styles.header}>
       <div className={styles.title}>
-    <h1> Portfolio </h1>
-  </div>
-  <div>
-    <ul className={styles.navigation}>
-    <li><a href=""></a>Home</li>
-    <li><a href=""></a>About me</li>
-    <li><a href=""></a>Projects</li>
-    <li><a href=""></a>Contact</li>
-  </ul>
-  </div>
-
-  </nav>
-);
-
-};
+        <h1> Portfolio </h1>
+      </div>
+      <div>
+        <ul className={styles.navigation}>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#home">About me</a>
+          </li>
+          <li>
+            <a href="#home">Projects</a>
+          </li>
+          <li>
+            <a href="#home">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
